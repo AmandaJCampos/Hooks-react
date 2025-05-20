c336d0c2a22bdcf69123595b7d3d7c31be42b5e5
AmandaJCampos <amandajcampos8@gmail.com>
2025-05-20 15:39:49 -0300

    UseEffects / UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useState } from 'react';
+import UseEffect from "./Hooks/UseEffects";
+import UseState from "./Hooks/UseState";
 
 export default function App() {
-  // Hook (é em JavaScript)
-  const [isVisible, setVisible] = useState(false);
-
-  // Alterna entre as visibilidades (true ou false)
-  function toggleVisibilidade() { //alternarVisibilidade
-    setVisible(!isVisible);
-  }
-
   return (
-    // JSX, somente HTML, JavaScript dentro de {chaves}
-    <div>
-      <h1>React Hooks</h1>
-
-
-      {/* Botão para ícone senha mostrar/esconder : continuar clicando */}
-      <button onMouseDown={toggleVisibilidade} onMouseUp={toggleVisibilidade}>
-        {isVisible ? 'Esconder' : 'Mostrar'}
-      </button>
-
-      {isVisible && <p>Esta é a mensagem!</p>}
-    </div>
-  )
+    <>
+    {/* <UseState /> */}
+    <UseEffect />
+    </>
+  );
 }
